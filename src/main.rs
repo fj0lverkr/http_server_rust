ff73b924c7abbbd8e4c98fe581aad198ab41e827
fj0lverkr <nils.nahooy@live.com>
2020-12-21 20:15:54 +0100

    Set up enum Request

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,40 @@ impl Server {
     }
 
     fn run(self) {
-        println!("{}", self.addr);
+        println!("Listening on {}", self.addr);
     }
 }
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    //The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.
+    HEAD,
+    //The HEAD method asks for a response identical to that of a GET request, but without the response body.
+    POST,
+    //The POST method is used to submit an entity to the specified resource, often causing a change in state or side effects on the server.
+    PUT,
+    //The PUT method replaces all current representations of the target resource with the request payload.
+    DELETE,
+    //The DELETE method deletes the specified resource.
+    CONNECT,
+    //The CONNECT method establishes a tunnel to the server identified by the target resource.
+    OPTIONS,
+    //The OPTIONS method is used to describe the communication options for the target resource.
+    TRACE,
+    //The TRACE method performs a message loop-back test along the path to the target resource.
+    PATCH,
+    //The PATCH method is used to apply partial modifications to a resource. 
+
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
